9aeb1f63ac12cca29162b43882bb94cc9f26b330
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2021-08-08 11:45:52 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt update'
+sudo apt-get -y install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools'
+sudo apt update
+sudo apt-get -y install libmysqlclient-dev
+pip3 install wheel
+sudo apt -y install python3-venv
+python3 -m venv myprojectenv
+source myprojectenv/bin/activate
+pip install -r requirements/dev.txt
+export FLASK_APP=flasky.py
+export FLASK_DEBUG=1
