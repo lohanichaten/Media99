61cd21ff830916b6783a7c0139c23e9c0d1f4ee7
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2021-08-08 17:06:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ sudo apt-get -y install python3-pip python3-dev build-essential libssl-dev libff
 sudo apt update
 sudo apt-get -y install libmysqlclient-dev
 pip3 install wheel
-sudo apt-get install python3.6
 sudo apt -y install python3-venv
 python3 -m venv myprojectenv
 source myprojectenv/bin/activate
 pip install -r requirements/dev.txt
-export FLASK_APP=flasky.py
-export FLASK_DEBUG=1
+
